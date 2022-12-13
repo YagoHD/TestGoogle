84ba6d1e4feb85e6c033dfe8388dbc3244b1acd7
YagoHD <yagorb@hotmail.com>
2022-12-13 13:42:29 +0100

    ADD: Tests getActiveAndCompletedStats_Completed_returnsHundred & getActiveAndCompletedStats_TwoCompletedThreActives_returnsFortySixty

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -21,4 +21,37 @@ internal class StatisticsUtilsTest{
         assertThat(result.completedTasksPercent, `is` (0f))
         assertThat(result.activeTasksPercent, `is` (100f))
     }
+
+    @Test
+    fun getActiveAndCompletedStats_Completed_returnsHundred(){
+
+        //Creamos una tarea activa (false significa que no esta completa)
+        val tasks = listOf<Task>(
+            Task("titulo","descripcion", isCompleted = true)
+        )
+        //Llamamos a getActiveAndCompletedStats con la tarea antes creada
+        val result = getActiveAndCompletedStats(tasks)
+        //Comprobamos el resultado
+        assertThat(result.completedTasksPercent, `is` (100f))
+        assertThat(result.activeTasksPercent, `is` (0f))
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_TwoCompletedThreActives_returnsFortySixty(){
+
+        //Creamos una tarea activa (false significa que no esta completa)
+        val tasks = listOf(
+            Task("titulo","descripcion", isCompleted = true),
+            Task("titulo", "descripcion", isCompleted = true),
+            Task("titulo","descripcion", isCompleted = false),
+            Task("titulo", "descripcion", isCompleted = false),
+            Task("titulo", "descripcion", isCompleted = false)
+        )
+        //Llamamos a getActiveAndCompletedStats con la tarea antes creada
+        val result = getActiveAndCompletedStats(tasks)
+        //Comprobamos el resultado
+        assertThat(result.completedTasksPercent, `is` (40f))
+        assertThat(result.activeTasksPercent, `is` (60f))
+    }
+
 }
