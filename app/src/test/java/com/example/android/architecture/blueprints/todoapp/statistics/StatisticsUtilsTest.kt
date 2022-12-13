fcf8431dc0789a22eebbc09620734b94337a74be
YagoHD <yagorb@hotmail.com>
2022-12-13 13:28:37 +0100

    ADD: Created a test class, Write the first test function Parte del commit anterior, en el que por error un archivo quedo en Unstaged Files

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -1,5 +1,22 @@
 package com.example.android.architecture.blueprints.todoapp.statistics
 
-import org.junit.jupiter.api.Assertions.*
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import junit.framework.Assert.assertEquals
+import org.junit.Test
 
-internal class StatisticsUtilsTest
+internal class StatisticsUtilsTest{
+
+    @Test
+    fun getActiveAndCompletedStats_noCompleted_returnsZero(){
+
+        //Creamos una tarea activa (false significa que no esta completa)
+        val tasks = listOf<Task>(
+            Task("titulo","descripcion", isCompleted = false)
+        )
+        //Llamamos a getActiveAndCompletedStats con la tarea antes creada
+        val result = getActiveAndCompletedStats(tasks)
+        //Comprobamos el resultado
+        assertEquals(result.completedTasksPercent, 0f)
+        assertEquals(result.activeTasksPercent, 100f)
+    }
+}
